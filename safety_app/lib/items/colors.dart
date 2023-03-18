f0afe8dc72f9a98ca4cd926249294b7cb0227634
Amara <idhikashjaishankar1@gmail.com>
2023-03-18 13:02:51 +0530

    lil edits here and there

--- safety_app/lib/items/colors.dart
+++ safety_app/lib/items/colors.dart
@@ -3,5 +3,6 @@ import 'package:flutter/material.dart';
 Color backgroundColor = Colors.black;
 Color primaryHighlight = Color.fromARGB(255, 90, 165, 250);
 Color lowGrey = Color.fromARGB(255, 69, 69, 69);
+Color chatGrey = Color.fromARGB(255, 147, 147, 147);
 Color bottomNavBarColor = Color.fromARGB(255, 0, 0, 0);
 Color foregroundText = Colors.white;
